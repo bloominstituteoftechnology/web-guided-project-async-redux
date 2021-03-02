31179771f2e0f1da9fe4a40d5574480672f0265c
Josh Jacobson <joshjacobson14@gmail.com>
2021-03-01 17:36:37 -0800

    lecture work-in-progress

---
+++ thunk/src/reducers/titleReducer.js
@@ -0,0 +1,20 @@
+// Define initial state
+const initialState = {};
+
+export const UPDATE_TITLE = "UPDATE_TITLE";
+export const TOGGLE_EDITING = "TOGGLE_EDITING";
+
+
+// Define a reducer with all state management functions related to the title
+
+export const titleReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case UPDATE_TITLE:
+            return { ...state }
+        case TOGGLE_EDITING: 
+            return { ...state }
+        default:
+            console.log("Error: unknown action type in Title Reducer");
+            return state;  
+    }
+}
