f4df3438f52dff1fb4ba11f3c78e4f94fdbe04c8
Josh Jacobson <joshjacobson14@gmail.com>
2021-03-01 17:39:53 -0800

    lecture work-in-progress

---
+++ thunk/src/actions/titleActions.js
@@ -0,0 +1,10 @@
+export const UPDATE_TITLE = "UPDATE_TITLE";
+export const TOGGLE_EDITING = "TOGGLE_EDITING";
+
+export const updateTitle = (newTitle) => {
+
+}
+
+export const toggleEditing = () => {
+
+}
