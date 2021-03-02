5f9591d998bc7edf936b328d740f2f97dd816d9e
Josh Jacobson <joshjacobson14@gmail.com>
2021-03-01 17:10:37 -0800

    lecture work-in-progress

--- middleware-example/src/index.js
+++ middleware-example/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import logger from "redux-logger";
 
