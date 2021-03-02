960c2a3c3c66c184330b4e832b4adea59bc3cf30
Josh Jacobson <joshjacobson14@gmail.com>
2021-03-01 18:13:22 -0800

    lecture work-in-progress

--- thunk/src/actions/index.js
+++ thunk/src/actions/index.js
@@ -1,6 +1,9 @@
 export const UPDATE_TITLE = "UPDATE_TITLE";
 export const TOGGLE_EDITING = "TOGGLE_EDITING";
 export const FETCHING_QUOTE_START = "FETCHING_QUOTE_START";
+export const FETCHING_QUOTE_SUCCESS = "FETCHING_QUOTE_SUCCESS;
+export const FETCHING_QUOTE_FAILURE = "FETCHING_QUOTE_FAILURE";
+
 
 export const updateTitle = (newTitle) => {
     return {type: UPDATE_TITLE, payload: newTitle}
@@ -12,7 +15,7 @@ export const toggleEditing = () => {
 
 export const getJoke = (dispatch) => {
     // update state to loading
-    dispatch({type: FETCHING_QUOTE_START})
+    dispatch({ type: FETCHING_QUOTE_START });
 
     // begin an API request
 
