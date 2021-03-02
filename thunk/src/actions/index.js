1fd0cca4347b48800dd85f00a245592ef67c9cd6
Josh Jacobson <joshjacobson14@gmail.com>
2021-03-01 18:31:25 -0800

    lecture work-in-progress

--- thunk/src/actions/index.js
+++ thunk/src/actions/index.js
@@ -14,12 +14,16 @@ export const toggleEditing = () => {
     return {type: TOGGLE_EDITING}
 }
 
+const headers = {
+    Accept: "application/json"
+}
+
 export const getJoke = () => (dispatch) => {
     // update state to loading
     dispatch({ type: FETCHING_QUOTE_START });
 
     // begin an API request
-    axios.get("https://icanhazdadjoke.com/")     // respond to happy path & sad path, updating state with API response
+    axios.get("https://icanhazdadjoke.com/", {headers})     // respond to happy path & sad path, updating state with API response
     .then(res => {
         console.log(res);
     })
