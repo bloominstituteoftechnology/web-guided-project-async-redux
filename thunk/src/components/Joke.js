81d890571039a39a4fadab6a0b5094d462c3f2ca
Josh Jacobson <joshjacobson14@gmail.com>
2021-03-01 18:28:57 -0800

    lecture work-in-progress

--- thunk/src/components/Joke.js
+++ thunk/src/components/Joke.js
@@ -1,16 +1,19 @@
+import { map } from "async";
 import React from "react";
+import { connect } from "react-redux";
 import { getJoke } from "../actions";
 
 const Joke = () => {
     return (
         <>
             <h2>Dad says:</h2>
-            <button>Get new joke</button>
+            <button onClick={props.getJoke()}>Get new joke</button>
         </>
     )
 }
 
-const mapStateToProps = () => {
+// Step 3 (again): connect components
+const mapStateToProps = (state) => {
     return {
         joke: state.joke,
         error: state.error,
@@ -20,4 +23,4 @@ const mapStateToProps = () => {
 
 const mapDipatchToProps = {getJoke}
 
-export default Joke;
+export default connect(mapStateToProps,mapDipatchToProps)(Joke);
